fc920ea889cdf6ac0dee6ce537d72b56ea603838
Sagar Yadav <sagaryadav17@outlook.com>
2021-09-12 20:06:41 +0530

    Layout Updated
    Added Nord Color Schema, Added Tailwind CSs and Added Badges for Skills

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Contact Links
+const linkedin = document.getElementById('linkedin').href = 'https://linkedin.com/in/sagaryadav17';
+const github = document.getElementById('github').href = 'https://github.com/sagaryadav17';
+const hashnode = document.getElementById('hashnode').href = 'https://sagaryadav17.hashnode.dev/';
+const outlook = document.getElementById('outlook').href = 'mailto:sagaryadav17@outlook.com';
+const twitter = document.getElementById('twitter').href = 'https://twitter.com/deadwaist';
+
+// Skills Icons
+const python = document.getElementById('python').src = 'https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white';
+const javascript = document.getElementById('javascript').src = 'https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E';
+const typescript = document.getElementById('typescript').src = 'https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white';
+const django = document.getElementById('django').src = 'https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=white';
+const djangorestframework = document.getElementById('djangorestframework').src = 'https://img.shields.io/badge/DJANGO-REST-ff1709?style=for-the-badge&logo=django&logoColor=white&color=ff1709&labelColor=gray';
+const fastapi = document.getElementById('fastapi').src = 'https://img.shields.io/badge/fastapi-109989?style=for-the-badge&logo=FASTAPI&logoColor=white';
+const flask = document.getElementById('flask').src = 'https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=flask&logoColor=white';
+const postgresql = document.getElementById('postgresql').src = 'https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white';
+const mysql = document.getElementById('mysql').src = 'https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white';
+const mongodb = document.getElementById('mongodb').src = 'https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white';
+const redis = document.getElementById('redis').src = 'https://img.shields.io/badge/redis-%23DD0031.svg?&style=for-the-badge&logo=redis&logoColor=white';
+const git = document.getElementById('git').src = 'https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white';
+const docker = document.getElementById('docker').src = 'https://img.shields.io/badge/Docker-2CA5E0?style=for-the-badge&logo=docker&logoColor=white';
+const conda = document.getElementById('conda').src = 'https://img.shields.io/badge/conda-342B029.svg?&style=for-the-badge&logo=anaconda&logoColor=white';
+const heroku = document.getElementById('heroku').src = 'https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white';
+const linux = document.getElementById('linux').src = 'https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black';
